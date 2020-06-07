e1bbae05a11d3ed2b37e592cf481f1cbebe6d677
Steve Klein <steve.klein@strategiced.com>
2020-06-07 14:37:41 -0500

    before changing to cron job

--- bin.js
+++ bin.js
@@ -1,13 +1,24 @@
 const request = require('request');
 const moment = require('moment');
+const package = require('./package.json');
+const cron = require('node-cron');
+
+const link = 'https://www.kickstarter.com/projects/228767892/open-ocean/?ref=kicktraq';
 
 const clickIt = () => {
   const intervalInMilliseconds = 86400000;//24 hours
-  request("https://www.kickstarter.com/projects/228767892/open-ocean/?ref=kicktraq", function (error, response, body) {
+  request(link, function (error, response, body) {
     if (error) {
       console.error(new Error(error));
     } else {
-      console.log(`I clicked kicktraq for https://www.kickstarter.com/projects/228767892/open-ocean/?ref=kicktraq at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`,'\n',`I'll click again on ${moment().add(intervalInMilliseconds, 'milliseconds').format('MMMM Do YYYY, h:mm:ss a')} `);
+      console.log(` ******************************************`, `\n`,
+        `*    Thanks for using ${package.name}!     *`, `\n`,
+        `*                                        *`, `\n`,
+        `******************************************`, `\n`,
+        `I clicked kicktraq for: `, `\n`, `${link} `, `\n`, `${moment().format('MMMM Do YYYY')} at ${moment().format('h:mm:ss a')}`, '\n\n',
+        
+        `I'll click again:`, `\n`,
+        `${moment().add('hours',24).format('MMMM Do YYYY')} at ${moment().format('h:mm:ss a')}`, '\n\n');
     }
   });
   setTimeout(clickIt, intervalInMilliseconds);
