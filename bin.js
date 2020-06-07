00605bbdb7ba451d785f4d18db8cc717ffe7a52c
Steve Klein <steve.klein@strategiced.com>
2020-06-07 14:53:43 -0500

    adding cron schedule for same hour/minute daily"

--- bin.js
+++ bin.js
@@ -6,22 +6,24 @@ const cron = require('node-cron');
 const link = 'https://www.kickstarter.com/projects/228767892/open-ocean/?ref=kicktraq';
 
 const clickIt = () => {
-  const intervalInMilliseconds = 86400000;//24 hours
+  console.log(` ******************************************`, `\n`,
+        `*    Thanks for using ${package.name}!     *`, `\n`,
+        `*                                        *`, `\n`,
+        `******************************************`, `\n`)
   request(link, function (error, response, body) {
     if (error) {
       console.error(new Error(error));
     } else {
-      console.log(` ******************************************`, `\n`,
-        `*    Thanks for using ${package.name}!     *`, `\n`,
-        `*                                        *`, `\n`,
-        `******************************************`, `\n`,
-        `I clicked kicktraq for: `, `\n`, `${link} `, `\n`, `${moment().format('MMMM Do YYYY')} at ${moment().format('h:mm:ss a')}`, '\n\n',
+      console.log(
+        `I clicked kicktraq for: `, `\n`, `${link} `, `\n`, `${moment().format('MMMM Do YYYY')} at ${moment().format('h:mm a')}`, '\n\n',
         
         `I'll click again:`, `\n`,
-        `${moment().add('hours',24).format('MMMM Do YYYY')} at ${moment().format('h:mm:ss a')}`, '\n\n');
+        `${moment().add(24,'hours').format('MMMM Do YYYY')} at ${moment().format('h:mm a')}`, '\n\n');
     }
   });
-  setTimeout(clickIt, intervalInMilliseconds);
+  cron.schedule(`${moment().minutes()} ${moment().hour()} * * *`, () => { //this is cron syntax for every at current hour and minute
+    clickIt();
+  })
 }
 
 clickIt();
